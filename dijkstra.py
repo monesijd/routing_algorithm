3260c2fd58a64a243ba597e13dd43cf47051f92c
Brian <brian950804@gmail.com>
2024-12-26 22:51:28 +0800

    Simplify output form.

--- dijkstra.py
+++ dijkstra.py
@@ -8,42 +8,10 @@ def find_min_distance():
                 min_distance_index = index
     return min_distance_index
 
-num = int(input("Number of Nodes: "))
-route = list()
-len_table = [[float('inf'), None] for _ in range(num)]
-is_visited = [False] * num
-
-while True:
-    content = input()
-    if content == '0': break
-    route.append(list(map(int, content.split())))
-
-start = int(input("Please input start: "))
-len_table[start] = [0, start]
-
-for end in range(num):
-    if end == start: continue
-    node = start
-
-    while True:
-        for each_route in route:
-            if node == each_route[0]: next = each_route[1]
-            elif node == each_route[1]: next = each_route[0]  
-            else: continue
-
-            if len_table[next][0] > each_route[2] + len_table[node][0]:
-                len_table[next][0] = each_route[2] + len_table[node][0]
-                len_table[next][1] = node
-
-        is_visited[node] = True
-
-        node = find_min_distance()
-
-        if node == end or node == None:
-            break
-
+def print_table():
     print("node       | ", end=" ")
-    for i in range(num): print(chr(ord('A') + i), end="  ") 
+    for i in range(num): 
+        print(chr(ord('A') + i), end="  ") 
     print() 
 
     print("min dis    | ", end=" ")
@@ -60,4 +28,47 @@ for end in range(num):
         else: last_hop = chr(ord('A') + len_table[each_end][1])
         print(f"{last_hop:3s}", end="")
     print("\n--------------------------------------------")
-    
+ 
+
+num = int(input("Number of Nodes: "))
+route = list()    
+# 記錄到每一條邊的距離
+
+len_table = [[float('inf'), None] for _ in range(num)]    
+# 記錄到起點的長度
+
+is_visited = [False] * num    
+# 紀錄是否有訪問過
+
+while True:
+    content = input()
+    if content == '0': break
+    route.append(list(map(int, content.split())))
+
+start = int(input("Please input start: "))
+len_table[start] = [0, start]
+times = 0
+
+for end in range(num):
+    if end == start: continue
+    node = start
+
+    while True:
+        for each_route in route:
+            if node == each_route[0]: next = each_route[1]
+            elif node == each_route[1]: next = each_route[0]  
+            else: continue
+
+            if len_table[next][0] > each_route[2] + len_table[node][0]:
+                len_table[next][0] = each_route[2] + len_table[node][0]
+                len_table[next][1] = node
+            times += 1
+
+        is_visited[node] = True
+
+        node = find_min_distance()
+        if node == end or node == None:
+            break
+
+print_table()
+print(f"Complete with {times} rounds.")
